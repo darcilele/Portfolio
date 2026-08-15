ef06d586f07277cb32a312d5ba487124c5adfa2a
Guilherme coelho <guilhermecoelhogama@gmail.com>
2026-08-14 21:16:02 -0300

    configure GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
