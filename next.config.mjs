0d636af6316ca12222f18432e014e6d76cafb505
Guilherme coelho <guilhermecoelhogama@gmail.com>
2026-08-14 21:31:26 -0300

    fix github pages paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
   trailingSlash: true,
   images: {
     unoptimized: true,
